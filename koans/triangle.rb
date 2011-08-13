febcb863b090609ecb26650fc52dd215ea411f5f
Eli Gundry <eligundry@ubuntu.(none)>
2011-08-13 14:42:16 -0400

    My inital progress

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,34 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	t = Triangle.new(a, b, c)
+	return t.type
+end
+
+class Triangle
+	def initialize(a, b, c)
+		@sides = [a, b, c].sort
+		guard_against_invalid_lengths
+	end
+
+	def type
+		case @sides.uniq.size
+		when 1 then :equilateral
+		when 2 then :isosceles
+		else :scalene
+		end
+	end
+
+	private
+	def guard_against_invalid_lengths
+		if @sides.any? { |x| x <= 0 }
+			raise TriangleError, "Sides must be greater than 0"
+		end
+
+		if @sides[0] + @sides[1] <= @sides[2]
+			raise TriangleError, "Not valid triangle lengths"
+		end
+	end
 end
 
 # Error class used in part 2.  No need to change this code.
